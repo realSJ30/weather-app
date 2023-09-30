3f6162434f9e0b9e6ba0edbceee7385bdb6fa9b2
realSJ30 <realsjmoraga@gmail.com>
2023-09-30 13:55:48 +0800

    testing branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Nav from "./components/Nav";
 import { RouteList } from "./routes";
@@ -7,6 +9,32 @@ const App = () => {
   const { isAuthenticated, isLoading } = useAuth0();
   const { routes } = RouteList({ isAuthenticated, isLoading });
   const router = createBrowserRouter(routes);
+
+  const testNeon = async () => {
+    const orgId = "elihuclub";
+    const apiKey = "0c385f8d02639b43e655d78892d48508";
+
+    var config = {
+      method: "GET",
+      url: "https://api.neoncrm.com/v2/accounts?userType=INDIVIDUAL",
+      headers: {
+        Authorization: `Basic ${btoa(`${orgId}:${apiKey}`)}`,
+      },
+    };
+    // Use the Axios library to make the API call
+    await axios(config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    testNeon();
+  }, []);
+
   return (
     <div className="font-roboto">
       <Nav />
